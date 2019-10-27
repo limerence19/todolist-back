189ca877ad30cbf2f7f1b2345fdd88371635a8fc
allenshu <mamba20160414@163.com>
2019-10-27 20:29:20 +0800

    update db url

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   development: {
-    port: 3200,
+    port: 8081,
     db: 'mongodb://localhost:27017/todolist'
   },
   production: {
-    port: 3200,
-    db: 'mongodb://todolist_runner:F**kcherish@127.0.0.1:3200/todolist'
+    port: 3300, // node application port
+    db: 'mongodb://todolist_runner:F**kcherish@101.132.68.200:18955/todolist'
   }
 }
